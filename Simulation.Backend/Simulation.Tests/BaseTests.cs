27d62a31f204ce361de6779ac1e960fa86b28c7f
German <german.nikolishin@gmail.com>
2020-07-30 21:33:05 +0300

    updated tests

--- Simulation.Backend/Simulation.Tests/BaseTests.cs
+++ Simulation.Backend/Simulation.Tests/BaseTests.cs
@@ -30,7 +30,8 @@ namespace Simulation.Tests
             var output = Simulation.InternalRun();
             foreach (var o in output)
             {
-                Assert.IsTrue(o.VehicleState.Height > 100);
+                Assert.IsTrue(o.VehicleState.Height > 0);
+                Assert.IsTrue(o.VehicleState.Velocity > 0);
             }
         }
 
@@ -39,6 +40,37 @@ namespace Simulation.Tests
         {
             Simulation.SpaceVehicle.FlyNextTimeInterval(10);
             Assert.IsTrue(Simulation.SpaceVehicle.AngleBelowHorizontal > 0);
+        }
+        
+        [TestMethod]
+        public void ChangeAngleOfAttack() 
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Simulation.SpaceVehicle.AngleOfAttack += i;
+                RunSimulation();
+            }
+        }
+
+        [TestMethod]
+        public void TestMass()
+        {
+            int mass = Simulation.SpaceVehicle.Mass;
+            while (Simulation.SpaceVehicle.Mass > 10000)
+            {
+                mass -= 1000;
+                var conditions = new InitialConditionsDTO()
+                {
+                    AngleBelowHorizontal = 6.93,
+                    AngleOfAttack = 20,
+                    Height = 80000,
+                    Mass = mass,
+                    Radius = 6.6,
+                    Velocity = 8000
+                };
+                Simulation.SetupEnvironment(5, conditions, Core.Simulation.VehicleType.SpaceShuttle);
+                RunSimulation();
+            }
         }
     }
 }
